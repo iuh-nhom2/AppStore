3918cdde84337f5c0f3ef8cc583b6b67883e2517
letuankietgmtco <letuankietdhth@gmail.com>
2018-07-04 10:19:17 +0700

    update
    update

--- QLSHOP/Router.js
+++ QLSHOP/Router.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import{
+import {
 	View,
 	Text,
 	Icon
-}from 'react-native';
-import {StackNavigator, TabNavigator} from 'react-navigation';
+} from 'react-native';
+import { StackNavigator, TabNavigator } from 'react-navigation';
 import Login from './Component/Main/Login.js';
 import Customer from './Component/Main/CustomerView.js';
 import Welcome from './Component/welcome.js';
@@ -12,95 +12,134 @@ import Major from './Component/Main/Majorview.js';
 import Majorshows from './Component/Main/Majorshows.js';
 import User from './Component/Main/User.js';
 import About from './Component/Main/About.js';
+import AddReparation from './Component/Main/AddReparation.js';
+import Search from './Component/Main/Search.js';
+
+import CustomerView from './Component/Main/CustomerView';
 import Majoricon from './media/major.png';
 import Homeicon from './media/inconhome.png';
+import OrderReparation from './Component/Main/OrderReparation.js';
 export const HomeStack = StackNavigator({
-	
 
-	
-	ManHinh_WelCome:{
-		screen:Welcome,
-		
+	ManHinh_Majorview: {
+		screen: Major,
+		navigationOptions: {
+
+			headerStyle: {
+				height: 0
+			}
+		}
 	},
-	ManHinh_Login:{
+
+	ManHinh_WelCome: {
+		screen: Welcome,
+
+	},
+	
+	ManHinh_Login: {
 		screen: Login,
 	},
-	
-	ManHinh_Majorview:{
-		screen: Major,
-		navigationOptions:{
-			
-			headerStyle:{
-				height:0
-			}
-		}
-	},
-	
-	
-	
+
+
+
+
+
+
 });
 export const ShopStack = StackNavigator({
-	
-	ManHinh_Majorview:{
+	ManHinh_Customer: {
+		screen: Customer,
+	},
+	ManHinh_Majorview: {
 		screen: Major,
+		navigationOptions: {
+
+			headerStyle: {
+				height: 0,
+				header: null,
+				tabBarVisible: false,
+			}
+		}
+	},
+
+
+	ManHinh_Addrepair: {
+		screen: AddReparation,
+		navigationOptions: {
+
+			headerStyle: {
+				height: 0,
+				header: null
+
+			}
+		}
+
+	},
+	ManHinh_ListReparation: {
+		screen: OrderReparation,
+		navigationOptions: {
+
+			headerStyle: {
+				height: 0,
+				header: null
+
+			}
+		}
+
+	},
+	ManHinh_Search:{
+		screen: Search,
 		navigationOptions:{
-			
 			headerStyle:{
 				height:0,
-				header: null,
-				tabBarVisible: false,
+				header:null
 			}
 		}
-	},
-	ManHinh_Majorshow:{
-		screen: Majorshows,
-	
-	},
-	ManHinh_Customer:{
-		screen:Customer,
-	},
+	}
+
+
 });
 export const Tabbar = TabNavigator({
-	  Home:{
-			screen: HomeStack,
-			navigationOptions: {
-				
-				header: null,
-				tabBarVisible: false,
-				
-			}
-
-	  },
-	  Major:{
-		screen:ShopStack,
+	Home: {
+		screen: HomeStack,
 		navigationOptions: {
-				
+
 			header: null,
-			tabBarVisible: true,
-			
+			tabBarVisible: false,
+
 		}
-	  },
-	  User:{
-		screen:User
-	  },
-	  ABout:{
-		  screen:About
-	  }
+
+	},
+	Major: {
+		screen: ShopStack,
+		navigationOptions: {
+
+			header: null,
+
+
+		}
+	},
+	User: {
+		screen: User
+	},
+	ABout: {
+		screen: About
+	},
 },
-{
-	  swipeEnabled:true,
-	  tabBarPosition:'bottom',
+	{
+		swipeEnabled: true,
+		tabBarPosition: 'bottom',
 
-	  tabBarOptions:{
-		
-		  style:{
-			  backgroundColor:'#dddddd'
-		  },
-		  labelStyle:{
-			  fontSize:8,
+		tabBarOptions: {
 
-		  },
-		  inactiveTintColor:'green',
-		  activeTintColor:'red'
-	  }
-});
+			style: {
+				backgroundColor: '#dddddd'
+			},
+			labelStyle: {
+				fontSize: 8,
+
+			},
+			inactiveTintColor: 'green',
+			activeTintColor: 'red'
+		}
+	});
